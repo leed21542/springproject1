456f35ec7a98829219df084895c535ea2bc08fb7
leed21542 <leed21542@naver.com>
2021-01-26 17:03:57 +0900

    lombok & db연동

--- src/main/java/com/example/springproject1/model/SearchParam.java
+++ src/main/java/com/example/springproject1/model/SearchParam.java
@@ -1,8 +1,10 @@
 package com.example.springproject1.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@Data //Lombok (Getter, Setter 등 모든 필요요소 추가해준다.)
+@AllArgsConstructor //모든 종류의 생성자 추가
 public class SearchParam { //파라미터를 객체로 받기 위한 클래스
 
     private String account;
